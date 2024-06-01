adf4ae3be3959869c96454a18f6834b835cb29ce
Anisha Lathwal <114800501+anishaLATHWAL@users.noreply.github.com>
2024-06-01 17:03:07 +0530

    Bit Magic

---
+++ Bit Magic/BitwiseOperators.java
@@ -0,0 +1,19 @@
+
+public class BitwiseOperators {
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 7;
+
+        System.out.println("a&b = " + (a & b));
+
+        System.out.println("a|b = " + (a | b));
+
+        System.out.println("a^b = " + (a ^ b));
+
+        System.out.println("~a = " + ~a);
+        
+        a &= b;
+        System.out.println("a= " + a);
+    }
+}
