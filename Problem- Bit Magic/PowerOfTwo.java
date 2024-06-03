aff76f5bf1cb94cf12a4849a9d54ed1d8e2d3645
Anisha Lathwal <114800501+anishaLATHWAL@users.noreply.github.com>
2024-06-03 18:10:00 +0530

    Bit Magic

---
+++ Problem- Bit Magic/PowerOfTwo.java
@@ -0,0 +1,18 @@
+
+public class PowerOfTwo {
+    // .....
+}
+
+class Solution{
+    
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n){
+        
+        // Your code here
+        if(n==0){
+            return false;
+        }
+        return ( n & (n-1))==0;
+    }
+    
+}
