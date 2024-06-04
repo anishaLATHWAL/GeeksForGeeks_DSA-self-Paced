94b3dea7117ba70b8e1599a089445ce699d7f443
Anisha Lathwal <114800501+anishaLATHWAL@users.noreply.github.com>
2024-06-04 18:38:33 +0530

    Update Recursion2.java

--- Recursion/Recursion2.java
+++ Recursion/Recursion2.java
@@ -1,5 +1,13 @@
 package Recursion;
 
 public class Recursion2 {
-    
+    static void fun(int n){
+        if (n==0)
+            return;
+        System.out.println("AnishaLathwal04");
+        fun(n-1);
+    }    
+    public static void main(String[] args){
+        fun(5);
+    }
 }
