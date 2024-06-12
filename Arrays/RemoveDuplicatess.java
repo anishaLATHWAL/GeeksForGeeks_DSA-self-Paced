badb77b14c3d3628b6b3b6695ae85e9ef741cc7a
Anisha Lathwal <114800501+anishaLATHWAL@users.noreply.github.com>
2024-06-12 19:11:33 +0530

    Arrays

---
+++ Arrays/RemoveDuplicatess.java
@@ -0,0 +1,35 @@
+public class RemoveDuplicatess {
+    static int remDuplicates(int arr[], int n){
+        int res=1;
+        for(int i=1;i<n;i++){
+            if(arr[res-1]!=arr[i]){
+                arr[res]=arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
+    public static void main(String args[]) 
+    { 
+       int arr[] = {10, 20, 20, 30, 30, 30}, n = 6;
+
+       System.out.println("Before Removal of duplicates: ");
+
+       for(int i = 0; i < n; i++)
+       {
+       		System.out.print(arr[i]+" ");
+       }
+
+       System.out.println();
+
+       n = remDuplicates(arr, n);
+
+       System.out.println("After Removal of duplicates: ");
+
+       for(int i = 0; i < n; i++)
+       {
+       		System.out.print(arr[i]+" ");
+       }
+
+    } 
+}
