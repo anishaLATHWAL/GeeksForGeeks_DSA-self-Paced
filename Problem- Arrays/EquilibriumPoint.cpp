3b5122fd87670421434444789e0c63c172895b97
Anisha Lathwal <114800501+anishaLATHWAL@users.noreply.github.com>
2024-06-25 13:25:34 +0530

    Equilibrium point

---
+++ Problem- Arrays/EquilibriumPoint.cpp
@@ -0,0 +1,23 @@
+// This commit includes only the largestAndSecondLargest function. 
+// The rest of the implementation and context are present in the 
+// GeeksforGeeks (GFG) code editor.
+
+int equilibriumPoint(long long arr[], int n) {
+
+        // Your code here
+        long long rs=0;
+        for (int i=0;i<n;i++){
+            rs+=arr[i];
+            
+        }
+        long long ls=0;
+        for(int i=0;i<n;i++){
+            rs-=arr[i];
+            if(ls==rs){
+                return i+1;
+            }
+            ls+=arr[i];
+        }
+        return -1;
+        
+    }
