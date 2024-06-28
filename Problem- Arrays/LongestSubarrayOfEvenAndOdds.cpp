5697b294ecbd64c9795a31dd123c23c1a4afb528
Anisha Lathwal <114800501+anishaLATHWAL@users.noreply.github.com>
2024-06-28 20:12:46 +0530

    Arrays

---
+++ Problem- Arrays/LongestSubarrayOfEvenAndOdds.cpp
@@ -0,0 +1,19 @@
+// This commit includes only the maxEvenOdd function. 
+// The rest of the implementation and context are present in the 
+// GeeksforGeeks (GFG) code editor.
+int maxEvenOdd(int arr[], int n) 
+    { 
+       //Your code here
+       int res=1,curr=1;
+       for(int i=1;i<n;i++){
+           if((arr[i] % 2 == 0 && arr[i - 1] % 2 != 0) || 
+            (arr[i] % 2 != 0 && arr[i - 1] % 2 == 0)){
+               curr++;
+               res = max(res,curr);
+           }
+           else{
+               curr=1;
+           }
+       }
+       return res;
+    } 
